9629acf9674fb80ce6da44e05aa6298b0356a580
小狼 <wuyunfeng0@gmail.com>
2017-12-10 10:52:43 +0800

    添加首页文章基本展现样式

--- src/component/livingroom/livingroom.jsx
+++ src/component/livingroom/livingroom.jsx
@@ -23,15 +23,57 @@ class LivingRoom extends React.Component {
 
   render() {
     let { articleList } = this.props
+    articleList = [
+      {
+        _id: 1,
+        title: '第一条诗句',
+        content: '<p>今天天气真不错呵呵</p>',
+        author: '小狼',
+        img: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1870247392,1316906891&fm=27&gp=0.jpg',
+        time: '1小时前'
+      },{
+        _id: 2,
+        title: '第一条诗句',
+        content: '<p>今天天气真不错呵呵</p>',
+        author: '小狼',
+        img: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1870247392,1316906891&fm=27&gp=0.jpg',
+        time: '1小时前'
+      },{
+        _id: 3,
+        title: '第一条诗句',
+        content: '<p>今天天气真不错呵呵</p>',
+        author: '小狼',
+        img: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1870247392,1316906891&fm=27&gp=0.jpg',
+        time: '1小时前'
+      },{
+        _id: 4,
+        title: '第一条诗句',
+        content: '<p>今天天气真不错呵呵</p>',
+        author: '小狼',
+        img: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1870247392,1316906891&fm=27&gp=0.jpg',
+        time: '1小时前'
+      },{
+        _id: 5,
+        title: '第一条诗句',
+        content: '<p>今天天气真不错呵呵</p>',
+        author: '小狼',
+        img: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1870247392,1316906891&fm=27&gp=0.jpg',
+        time: '1小时前'
+      }
+    ]
     return (
-      <div>
+      <div className="livingroom">
         {/* 文章列表 */}
-        <ul>
+        <h2>文章</h2>
+        <ul className="article-list">
           {
             articleList.map(val => {
               return (
                 <li key={ val._id }>
-                  <h2>{ val.title }</h2>
+                  <img src={val.img} alt="头像"/>
+                  <strong>小狼</strong>
+                  <time>{ val.time }</time>
+                  <h3>{ val.title }</h3>
                   <p dangerouslySetInnerHTML={{__html: val.content}}></p>
                 </li>
               )
